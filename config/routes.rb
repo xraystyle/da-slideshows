1076077ed591c5babaec5dc923a4f53470128121
xraystyle <xraystyle@xraystyle.com>
2015-01-06 16:18:37 -0800

    use erb2haml to convert devise views from erb to haml.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
 	
+	root "static_pages#home"
+
 	devise_for :users
 
-    get 'static_pages/home'
+	match "/users",				to: 'static_pages#home', via: 'get'
+    # get 'static_pages/home'
 
-    root "static_pages#home"
 
 # The priority is based upon order of creation: first created -> highest priority.
 # See how all your routes lay out with "rake routes".
