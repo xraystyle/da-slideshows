4a784b1695e47d1d65695c16c3419c2fe71fc126
xraystyle <xraystyle@xraystyle.com>
2015-01-13 12:57:53 -0800

    Add Slideshow relationship to User model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,16 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
 	devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
 
+	# Validations:
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(?:\.[a-z\d\-]+)*\.[a-z]+\z/i
 
 	validates :email, presence: true, format: VALID_EMAIL_REGEX, uniqueness: { case_sensitive: false }
 
 
 
+
+	# Relationships:
+	has_one :slideshow
 
 
 end
