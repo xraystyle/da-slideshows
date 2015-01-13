e0cfccc461be70120c65bf9efb64a78199f6e026
xraystyle <xraystyle@xraystyle.com>
2015-01-13 12:33:29 -0800

    Migrate DB, start on slideshow model spec.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150106235434) do
+ActiveRecord::Schema.define(version: 20150113201739) do
+
+  create_table "slideshows", force: true do |t|
+    t.string   "seed"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
--- spec/models/slideshow_spec.rb
+++ spec/models/slideshow_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Slideshow, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+	before { @slideshow = Slideshow.new(seed: "DFE52DC8-726A-2C57-FEA4-6A61E9DEA8B0") }
+
+	subject { @slideshow }
+
+
+
 end
