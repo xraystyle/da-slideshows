9f5bee866ad2ee272c4d28082b0d7ca017b622f6
xraystyle <xraystyle@xraystyle.com>
2015-01-13 12:44:02 -0800

    Write tests for Slideshow model.

--- spec/models/slideshow_spec.rb
+++ spec/models/slideshow_spec.rb
@@ -6,6 +6,32 @@ RSpec.describe Slideshow, :type => :model do
 
 	subject { @slideshow }
 
+	describe "should respond to the following methods" do
+
+		it { should respond_to(:seed) }
+
+		# Relationships to User and Deviations
+		it { should respond_to(:user) }
+		it { should respond_to(:deviations) }
+
+	end
+
+
+	describe "should be valid when created properly" do
+		it { should be_valid }
+	end
+
+
+	describe "when seed is not present" do
+		before { @slideshow.seed = "" }
+		it { should_not be_valid }
+	end
+
+
+	describe "when seed format is invalid" do
+		before { @slideshow.seed = "This tastes like Grandma." }
+		it { should_not be_valid }
+	end
 
 
 end
