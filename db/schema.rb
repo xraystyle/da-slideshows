9617a4643b930365162a30577327f7ae163d26ca
xraystyle <xraystyle@xraystyle.com>
2015-01-13 17:39:08 -0800

    Run migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150113205055) do
+ActiveRecord::Schema.define(version: 20150114005533) do
+
+  create_table "deviations", force: true do |t|
+    t.string   "url"
+    t.string   "title"
+    t.string   "author"
+    t.boolean  "mature"
+    t.string   "src"
+    t.string   "thumb"
+    t.string   "orientation"
+    t.string   "uuid"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "slideshows", force: true do |t|
     t.string   "seed"
