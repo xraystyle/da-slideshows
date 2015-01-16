b9f87288d2ad45d59845580ef2d39b00833d01ff
xraystyle <xraystyle@xraystyle.com>
2015-01-16 12:29:45 -0800

    Add note to migration file.

--- db/migrate/20150116010909_rename_join_table.rb
+++ db/migrate/20150116010909_rename_join_table.rb
@@ -1,6 +1,7 @@
 class RenameJoinTable < ActiveRecord::Migration
   def change
-
+  	# Join table name was backwards. Rails joins need to be in lexical order,
+  	# otherwise they won't be found automatically.
   	rename_table :slideshows_deviations, :deviations_slideshows
 
   end
