13d9ab10d8f20bd16b91b25432923b841d2a1f58
xraystyle <xraystyle@xraystyle.com>
2015-01-15 16:58:31 -0800

    Add HABTM relationships to Deviation and Slideshow models.

--- app/models/deviation.rb
+++ app/models/deviation.rb
@@ -14,6 +14,9 @@ class Deviation < ActiveRecord::Base
 	validates :uuid, presence: true
 
 
+	# Relationships:
+	has_and_belongs_to_many :slideshows
+
 
 
 
--- app/models/slideshow.rb
+++ app/models/slideshow.rb
@@ -1,7 +1,12 @@
 class Slideshow < ActiveRecord::Base
 
+	# Validations:
 	VALID_SEED_REGEX = /[A-F0-9]{8}(-[A-F0-9]{4}){3}-[A-F0-9]{12}/
 
 	validates :seed, presence: true, length: { is: 36 }, format: VALID_SEED_REGEX, uniqueness: true
+
+	# Relationships:
+	has_and_belongs_to_many :deviations
+
 
 end
