de4d080a2507542f7d78fcfd9155839aeff55bd8
xraystyle <xraystyle@xraystyle.com>
2015-02-03 13:19:54 -0800

    Get channel changer view displaying deviations.

--- app/controllers/slideshows_controller.rb
+++ app/controllers/slideshows_controller.rb
@@ -1,7 +1,50 @@
 class SlideshowsController < ApplicationController
+
+	before_action :authenticate_user!
+
+	@@wh = "00000000-0000-0000-0000-000000000001"
+
+
+
   def channels
+  	@user_seed = current_user.seed
+
+  	whats_hot = Slideshow.where(seed: @@wh).first
+
+  	@channels = whats_hot.deviations.map { |d| {thumb: d.thumb, uuid: d.uuid} unless d.mature? }.compact
+
+  	# @channels = whats_hot.deviations.map do |d|
+  	# 	next if d.mature?
+  	# 	{thumb: d.thumb, uuid: d.uuid}	
+  	# end.compact
+
+
   end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
   def slideshow
   end
+
+  def home
+  	
+  end
+
+
+
 end
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,7 @@
 class StaticPagesController < ApplicationController
+  
   def home
+  	render 'slideshows/home' if user_signed_in?
   end
+
 end
