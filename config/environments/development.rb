78022cc55dc4e18ed9af295d7c14ec34bb5fbc8a
xraystyle <xraystyle@xraystyle.com>
2015-03-15 18:34:37 -0700

    Clean up tests, write a few more. Login/logout and account creation feature specs are tested.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   config.action_mailer.default_url_options = { host: 'localhost', port: 9292 }
+  config.action_mailer.delivery_method = :smtp
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
@@ -13,8 +14,8 @@ Rails.application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
-  # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  # DO care if the mailer can't send.
+  config.action_mailer.raise_delivery_errors = true
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
