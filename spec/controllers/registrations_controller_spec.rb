ae8cd912e04ed064682dfef5dced9f1f8ce2ccc0
xraystyle <xraystyle@xraystyle.com>
2015-03-16 22:01:49 -0700

    Implement sending of notification emails for new signups.

---
+++ spec/controllers/registrations_controller_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe RegistrationsController, :type => :controller do
+
+  # describe "POSTing to the 'create' action" do
+  #   # it "creates a new user and sends notification emails" do
+  #   #   request.env['HTTPS'] = 'on'
+  #   #    # @request.env["devise.mapping"] = Devise.mappings[:user]
+  #   #   devise_scope :user do
+  #   #     u = FactoryGirl.attributes_for(:user)
+  #   #     post :create, user: u
+  #   #   end
+  #   #   expect_email WelcomeMailer.welcome(u[:email])
+  #   # end
+  # end
+
+end
--- spec/features/users/user_account_registration.rb
+++ spec/features/users/user_account_registration.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
+require 'sidekiq/testing'
 
 describe "New user signs up for an account" do
 
