822855376c233711791b7eb4d8310f6ee4bd5672
xraystyle <xraystyle@xraystyle.com>
2015-03-16 18:39:52 -0700

    Get email previews working in development environment.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,6 +14,8 @@ Rails.application.configure do
   # DO care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
 
+  config.action_mailer.preview_path = 'spec/mailers/previews/'
+
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
--- spec/mailers/welcome_mailer_spec.rb
+++ spec/mailers/welcome_mailer_spec.rb
@@ -5,18 +5,18 @@ RSpec.describe WelcomeMailer, :type => :mailer do
   describe "Welcome Email" do
     
     let(:user) { FactoryGirl.create(:user) }
-    let(:mail) { WelcomeMailer.delay.welcome(user.email) }
+    let(:mail) { WelcomeMailer.welcome(user.email) }
 
     it "renders the subject" do
       expect(mail.subject).to eq("Welcome to DA Slideshows!")
     end
 
     it "sends to the correct email address" do
-      expect(mail.to).to eq(user.email)
+      expect(mail.to).to eq([user.email])
     end
 
     it "is sent from the correct address" do
-      expect(mail.from).to eq('info@da-slidesho.ws')
+      expect(mail.from).to eq(['info@da-slidesho.ws'])
     end
 
   end
