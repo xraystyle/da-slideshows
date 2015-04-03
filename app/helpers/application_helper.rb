bc71e1d08be336fb32bfdb3893a617265574661b
xraystyle <xraystyle@xraystyle.com>
2015-04-02 23:40:19 -0700

    Style flash messages with bootstrap classes.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -51,6 +51,10 @@ module ApplicationHelper
 
   end
 
+  # Translate Rails flash into a Bootstrap class.
+  def bootstrap_class_for(flash_type)
+   { success: "alert-success", error: "alert-danger", alert: "alert-warning", notice: "alert-info" }[flash_type.to_sym] 
+  end
 
 
 end
