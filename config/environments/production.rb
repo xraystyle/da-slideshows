51d07b23f2057dd499f6830ad0f415ea03f66b7b
xraystyle <xraystyle@xraystyle.com>
2015-04-16 16:49:54 -0700

    Add mailer info to production.rb, config database.yml to read db pass from ENV.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,21 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  # Set up actionmailer with mailgun creds for production env.
+  config.action_mailer.default_url_options = { host: 'da-slidesho.ws' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.mailgun.org",
+    port: 587,
+    domain: ENV["MG_DOMAIN"],
+    # enable_starttls_auto: true,
+    user_name: ENV["MG_USERNAME"],
+    password: ENV["MG_PASSWORD"],
+    authentication: "plain"
+  }
+
+
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
@@ -58,7 +73,7 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = false
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
